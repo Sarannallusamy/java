8ec906968aa56c45edebdefc93031c8dcfb95db8
Sarannallusamy <94984942+Sarannallusamy@users.noreply.github.com>
2022-12-29 08:39:22 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+class Main {
+  public static void main(String[] args) {
+	String name ="saran";
+    long mobile_number = 8870793737;
+	//mobile_number = 7896;
+	float myfloat = 0.99f;
+	char mychar = 'S';
+	boolean mybool = true;
+    System.out.println(name);
+	System.out.println(mobile_number);
+	System.out.println(myfloat);
+	System.out.println(mychar);
+	System.out.println(mybool);
+	
+  }
+}
+
+
+
