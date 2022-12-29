041dc0c708f54df5c9f1e6bed964a9f24aaff6d7
Sarannallusamy <94984942+Sarannallusamy@users.noreply.github.com>
2022-12-29 08:45:37 +0530

    ok

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 class Main {
   public static void main(String[] args) {
-	String name ="saran";
-    long mobile_number = 8870793737;
+	String name ="test";
+    long mobile_number = 793737;
 	//mobile_number = 7896;
 	float myfloat = 0.99f;
 	char mychar = 'S';
