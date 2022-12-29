fc4082b24e7d78820195809e62a6c9644b69a366
Sarannallusamy <94984942+Sarannallusamy@users.noreply.github.com>
2022-12-29 08:42:28 +0530

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,18 @@
+class Variable{
+	public static void main(String[] args){
+		int x=10;
+		int y=20;
+		int z=x+y;
+		System.out.println(z);
+		String first_name = "Saran";
+		String last_name ="Nallusamy";
+		String full_name = first_name + last_name;
+		System.out.println(full_name);
+		
+		//another method
+		
+		int P,Q,R;
+		P=Q=R=30;
+		System.out.println(P+Q+R);
+	}
+}
